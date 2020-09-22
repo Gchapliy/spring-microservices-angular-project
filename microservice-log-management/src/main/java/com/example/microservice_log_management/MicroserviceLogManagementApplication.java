f766fee177dbac292ec911ae9bb15b6f46bc805d
Hryhorii <grishachapliy1@gmail.com>
2020-09-22 15:47:42 +0300

    log service

---
+++ microservice-log-management/src/main/java/com/example/microservice_log_management/MicroserviceLogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_log_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLogManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceLogManagementApplication.class, args);
+    }
+
+}
