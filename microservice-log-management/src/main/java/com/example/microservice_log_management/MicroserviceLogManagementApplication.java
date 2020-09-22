9815188d0dce4a1a90c408d278dd1188e3844567
Hryhorii <grishachapliy1@gmail.com>
2020-09-22 17:04:24 +0300

    log eureka client

--- microservice-log-management/src/main/java/com/example/microservice_log_management/MicroserviceLogManagementApplication.java
+++ microservice-log-management/src/main/java/com/example/microservice_log_management/MicroserviceLogManagementApplication.java
@@ -1,11 +1,17 @@
 package com.example.microservice_log_management;
 
+import com.example.microservice_log_management.config.CassandraConfig;
+import com.example.microservice_log_management.config.WebConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@Import({WebConfig.class, CassandraConfig.class})
 public class MicroserviceLogManagementApplication {
 
     public static void main(String[] args) {
