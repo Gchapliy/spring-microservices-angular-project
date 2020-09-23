76bd809d21ebff92c7f480876f3a675e621b4a58
Hryhorii <grishachapliy1@gmail.com>
2020-09-23 09:08:48 +0300

    microservice course management

---
+++ microservice-course-management/src/main/java/com/example/microservice_course_management/MicroserviceCourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_course_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCourseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCourseManagementApplication.class, args);
+    }
+
+}
