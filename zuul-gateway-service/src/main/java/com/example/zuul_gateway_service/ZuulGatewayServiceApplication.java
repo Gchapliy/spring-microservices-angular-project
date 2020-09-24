dfb3c2824501e3c06f26cef21c80cb8f6426e0ca
Hryhorii <grishachapliy1@gmail.com>
2020-09-24 12:27:06 +0300

    zuul configuration

--- zuul-gateway-service/src/main/java/com/example/zuul_gateway_service/ZuulGatewayServiceApplication.java
+++ zuul-gateway-service/src/main/java/com/example/zuul_gateway_service/ZuulGatewayServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.zuul_gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class ZuulGatewayServiceApplication {
 
     public static void main(String[] args) {
